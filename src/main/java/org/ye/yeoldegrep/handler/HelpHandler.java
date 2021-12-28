fe44b9db5f7ea3df361d08b620f08383800d87be
Jakllp <57708725+Jakllp@users.noreply.github.com>
2021-12-28 19:47:20 +0100

    sendHelp - manpage
    feat: added manpage resource
    feat: added sendHelp-Content

--- src/main/java/org/ye/yeoldegrep/handler/HelpHandler.java
+++ src/main/java/org/ye/yeoldegrep/handler/HelpHandler.java
@@ -1,16 +1,26 @@
 package org.ye.yeoldegrep.handler;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Class with Methods to handle all sort of helping things for users
  */
 public class HelpHandler {
 
     /**
-     * Method that sends the --help string (a man-page-excerpt)
+     * Method that sends the --help-string (a man-page-excerpt)
      */
     public static void sendHelp() {
-        System.out.println("""
-                """); //TODO Add actual content
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("manpage");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+        }
     }
 
     public static void printInstructions(){
