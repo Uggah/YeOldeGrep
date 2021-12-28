aa2f8f4c5a895b731fe89a5055ecadad8c3de066
Thyrexx <henry.russxy@gmail.com>
2021-12-28 14:50:01 +0100

    feat: impplemented Main class

--- src/main/java/org/ye/yeoldegrep/Main.java
+++ src/main/java/org/ye/yeoldegrep/Main.java
@@ -1,4 +1,23 @@
 package org.ye.yeoldegrep;
 
+import org.ye.yeoldegrep.parser.ArgumentParser;
+
+/**
+ * Main class which acts as interface between the other classes
+ */
 public class Main {
+    /**
+     * <p>Calls the search method with the parsed input arguments</p>
+     *
+     * @param args User input
+     */
+    public static void main(String[] args) {
+        try {
+            Search.search(ArgumentParser.getQuery(args), ArgumentParser.getOpts(args), ArgumentParser.getFilePaths(args));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
--- src/test/java/org/ye/yeoldegrep/MainTest.java
+++ src/test/java/org/ye/yeoldegrep/MainTest.java
@@ -2,7 +2,4 @@ package org.ye.yeoldegrep;
 
 public class MainTest {
 
-
-
-
 }
